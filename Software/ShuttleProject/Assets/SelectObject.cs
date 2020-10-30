25d385436c402d8a20a85169d3b0b14380e81099
Konne1 <64267327+Konne1@users.noreply.github.com>
2020-10-30 19:46:45 +0100

    Spawn a hand
    Added a Button to spawn a hand that is adapted to the egdes of the choosen object.

---
+++ Software/ShuttleProject/Assets/SelectObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour
+{
+    private GameObject go;
+    private RaycastHit hit;
+    private Ray ray;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                if (hit.transform)
+                {
+                    go = hit.transform.gameObject;
+                }
+            }
+        }
+    }
+
+    //return the clicked object
+    public GameObject getObject() => go;
+}
