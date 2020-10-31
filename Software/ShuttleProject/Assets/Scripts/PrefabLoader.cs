36b280aec05aad915a280618ee8e69ad45443dec
jsx15 <64267054+jsx15@users.noreply.github.com>
2020-10-31 17:04:14 +0100

    Add prefab loader

---
+++ Software/ShuttleProject/Assets/Scripts/PrefabLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Skripte
+{
+    public class PrefabLoader
+    {
+        public String[] getPrefab()
+        {
+            //Get default resource folder
+            String folderPath = Application.dataPath + "\\Resources\\";
+            //Get every .prefab file in folder
+            String[] files = Directory.GetFiles(folderPath, "*.prefab");
+            return files;
+        }
+    }
+}
