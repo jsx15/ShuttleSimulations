f2f9aa729a34f7e962d9860cb454703c784ebcd3
jsx15 <64267054+jsx15@users.noreply.github.com>
2020-12-14 12:29:36 +0100

    Add CarryIDManager

---
+++ Software/ShuttleProject/Assets/Scripts/Utility/CarryIDManager.cs
@@ -0,0 +1,32 @@
+﻿namespace Scripts
+{
+    public class CarryIDManager
+    {
+        /*
+         * Current IDs
+         */
+        private string _currentCarryIdLeft, _currentCarryIdRight, _currentCarryIdBoth;
+
+        /*
+         * Getter / Setter
+         */
+        public string CurrentCarryIdLeft
+        {
+            get => _currentCarryIdLeft;
+            set => _currentCarryIdLeft = value;
+        }
+
+        public string CurrentCarryIdRight
+        {
+            get => _currentCarryIdRight;
+            set => _currentCarryIdRight = value;
+        }
+
+        public string CurrentCarryIdBoth
+        {
+            get => _currentCarryIdBoth;
+            set => _currentCarryIdBoth = value;
+        }
+        
+    }
+}
