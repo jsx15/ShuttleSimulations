7f0b28fe99498a32bcb774fcc0e920e88ce9ba58
jsx15 <64267054+jsx15@users.noreply.github.com>
2020-12-26 11:19:22 +0100

    Add ESC-Menu

---
+++ Software/ShuttleProject/Assets/Scripts/UI/EscMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class EscMenu : MonoBehaviour
+    {
+        /// <summary>
+        ///     The boolean to represent status
+        /// </summary>
+        private bool _open = false;
+
+        /// <summary>
+        ///     The menu canvas
+        /// </summary>
+        public GameObject menuCanvas;
+
+        /// <summary>
+        ///     Check for esc key pressed
+        /// </summary>
+        public void Update()
+        {
+            if (!Input.GetKey(KeyCode.Escape)) return;
+            _open = true;
+            menuCanvas.SetActive(true);
+        }
+
+        /// <summary>
+        ///    Exit the application
+        /// </summary>
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        /// <summary>
+        ///     Hide menu canvas -> return to application
+        /// </summary>
+        public void Continue()
+        {
+            _open = false;
+            menuCanvas.SetActive(false);
+        }
+        
+    }
+}
