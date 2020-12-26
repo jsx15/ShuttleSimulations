2cbfe19b595032460eabb5f6aac1d42f47bf69ef
jsx15 <64267054+jsx15@users.noreply.github.com>
2020-12-26 14:49:31 +0100

    Add help and esc menu function

--- Software/ShuttleProject/Assets/Scripts/UI/EscMenu.cs
+++ Software/ShuttleProject/Assets/Scripts/UI/EscMenu.cs
@@ -1,27 +1,89 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Threading;
+using UnityEngine;
 
 namespace Scripts
 {
     public class EscMenu : MonoBehaviour
     {
         /// <summary>
-        ///     The boolean to represent status
+        ///     The main menu canvas
         /// </summary>
-        private bool _open = false;
+        public GameObject escMenu;
 
         /// <summary>
-        ///     The menu canvas
+        ///     The help menu canvas
         /// </summary>
-        public GameObject menuCanvas;
+        public GameObject helpMenu;
+
+        /// <summary>
+        ///    The background
+        /// </summary>
+        public GameObject background;
+
+        /// <summary>
+        ///     Stores current state
+        /// </summary>
+        private State _currentState = State.Running;
+
+        /// <summary>
+        ///     Block input
+        /// </summary>
+        private bool _block;
 
         /// <summary>
         ///     Check for esc key pressed
         /// </summary>
         public void Update()
         {
-            if (!Input.GetKey(KeyCode.Escape)) return;
-            _open = true;
-            menuCanvas.SetActive(true);
+            //Check if input is blocked
+            if (_block) return;
+            
+            //Check if esc key is pressed
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                //Start block input
+                new Thread(BlockInput).Start();
+                
+                //Check current state
+                switch (_currentState)
+                {
+                    //Show esc menu
+                    case State.Running:
+                        ActivateState(State.Esc);
+                        break;
+                    
+                    //Exit esc menu
+                    case State.Esc:
+                        ExitState();
+                        break;
+                    
+                    //Exit help menu
+                    case State.Help:
+                        ExitState();
+                        break;
+                }
+                
+            }
+
+        }
+
+        /// <summary>
+        ///     Click listener for help button
+        /// </summary>
+        public void HelpButtonPressed()
+        {
+            if (_currentState is State.Running)
+            {
+                ActivateState(State.Help);
+                Thread t1 = new Thread(BlockInput);
+                t1.Start();
+            }
+
+            else if (_currentState is State.Help)
+            {
+                ExitState();
+            }
         }
 
         /// <summary>
@@ -37,9 +99,60 @@ namespace Scripts
         /// </summary>
         public void Continue()
         {
-            _open = false;
-            menuCanvas.SetActive(false);
+            ExitState();
+            
         }
+
+        /// <summary>
+        ///     Show state
+        /// </summary>
+        /// <param name="state">State to show</param>
+        private void ActivateState(State state)
+        {
+            background.SetActive(true);
+            switch (state)
+            {
+                case State.Esc:
+                    escMenu.SetActive(true);
+                    break;
+                case State.Help:
+                    helpMenu.SetActive(true);
+                    break;
+            }
+            _currentState = state;
+        }
+
+        /// <summary>
+        ///     Return to running state
+        /// </summary>
+        private void ExitState()
+        {
+            background.SetActive(false);
+            switch (_currentState)
+            {
+                case State.Esc:
+                    escMenu.SetActive(false);
+                    break;
+                case State.Help:
+                    helpMenu.SetActive(false);
+                    break;
+            }
+            _currentState = State.Running;
+
+        }
+        
+        private enum State
+        {
+            Running, Help, Esc
+        }
+
+        private void BlockInput()
+        {
+            _block = true;
+            Thread.Sleep(100);
+            _block = false;
+        } 
+        
         
     }
 }
