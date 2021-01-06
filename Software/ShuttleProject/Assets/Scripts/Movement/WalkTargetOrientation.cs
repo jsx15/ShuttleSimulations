8c2f9b3eaa0e12f521344f7cf211169d743221dd
Konne1 <64267327+Konne1@users.noreply.github.com>
2021-01-06 11:21:11 +0100

    Documentary of the WalkTargetOrientation class

--- Software/ShuttleProject/Assets/Scripts/Movement/WalkTargetOrientation.cs
+++ Software/ShuttleProject/Assets/Scripts/Movement/WalkTargetOrientation.cs
@@ -5,15 +5,24 @@ namespace Movement
 {
     public class WalkTargetOrientation : MonoBehaviour
     {
+        /// <summary>
+        ///     The parent of the WalkTarget
+        /// </summary>
         private Transform _parent;
+        
+        /// <summary>
+        ///     The direction from the WalkTarget to the _parent object
+        /// </summary>
         private Vector3 _direction;
         
+        // Start is called once per start
         private void Start()
         {
             _parent = this.transform.parent;
             _direction = this.transform.position - _parent.transform.position;
         }
 
+        // Update is called once per frame
         private void Update()
         {
             this.transform.LookAt(_parent, _direction);
